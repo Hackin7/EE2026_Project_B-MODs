7708796d1908c31270e41b50cfa0c9e7d1eadd36
Terence Chan Zun Mun <zunmun@gmail.com>
2024-03-17 12:41:39 +0800

    feat: task d integration

--- integration_group_basic_tasks/integration_group_basic_tasks.srcs/sources_1/new/adaptor_task_d.v
+++ integration_group_basic_tasks/integration_group_basic_tasks.srcs/sources_1/new/adaptor_task_d.v
@@ -2,23 +2,9 @@ module adaptor_task_d(
     input reset, input clk,
     input btnC, btnU, btnL, btnR, btnD, input [15:0] sw, output [15:0] led,
     output [6:0] seg, output dp, output [3:0] an,
-    inout [7:0] JB
+    // OLED
+    input [12:0] oled_pixel_index, output reg [15:0] oled_pixel_data
 );
-
-    wire [7:0] Jb;
-    assign JB[7:0] = Jb;
-
-    wire clk_6_25mhz;
-    clk_counter #(16, 5) clk6p25m (clk, clk_6_25mhz);
-
-    reg [15:0] oled_pixel_data = 16'h0000;
-    wire [12:0] oled_pixel_index;
-    wire [15:0] pixel_data = oled_pixel_data;
-    Oled_Display display(
-        .clk(clk_6_25mhz), .reset(0), 
-        .frame_begin(), .sending_pixels(), .sample_pixel(), .pixel_index(oled_pixel_index), .pixel_data(pixel_data), 
-        .cs(Jb[0]), .sdin(Jb[1]), .sclk(Jb[3]), .d_cn(Jb[4]), .resn(Jb[5]), .vccen(Jb[6]), .pmoden(Jb[7])
-    );
 
     reg [7:0] square_xpos = 0, square_ypos = 0;
     reg [15:0] square_color = 16'h001F;
